dd620d73c12dd6721fda6b214d35890544997020
WizaNi <wizalevi01@gmail.com>
2026-03-02 13:20:37 +0100

    Implement basic calculator functions in main.js

--- Web/main.js
+++ Web/main.js
@@ -1,1 +1,40 @@
+const display = document.getElementById('display');
+//5 == '5' //true
+//5 === '5' //false
+function appendToDisplay(input){
+    if (display.value === "Error" || display.value === "∞") {
+        clearDisplay();
+    }
+    display.value += input;
+}
 
+function erase() {
+    display.value = display.value.slice(0, -1);
+}
+
+function clearDisplay(){
+    display.value = "";
+}
+//console.log(eval("2*(3+2)"));
+function calculate() {
+    try{
+        let result = eval(display.value);
+        if (!isFinite(result)) {
+            display.value = "∞";
+        }
+        else{
+            display.value = result;
+        }  
+    }
+    catch (error){
+        display.value = "Error";
+    }
+    
+}
+
+document.addEventListener('keydown', (event)=>{
+    const key = event.key;
+    if (!isNaN(key)) {
+        appendToDisplay(key);
+    }
+})
